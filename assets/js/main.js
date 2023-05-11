47c9ecf4488aafe6666f4f9490372b0221593af4
lllytnik <papa.carlo.link@gmail.com>
2023-05-11 09:15:27 +0400

    homework-1 layout

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+// let count = 1;
+// const counterEl = document.getElementById("counter");
+// const resultEl = document.getElementById("result");
+
+// function multiply(num) {
+//     return num * 129;
+// }
+
+// function increment() {
+//     count++;
+//     counterEl.textContent = count;
+//     resultEl.textContent = multiply(count);
+// }
+
+// function decrement() {
+//     if (count > 0) {
+//         count--;
+//         counterEl.textContent = count;
+//         resultEl.textContent = multiply(count);
+//     }
+// }
