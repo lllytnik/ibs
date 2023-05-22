27b09d09744825c70e1c39d0928f813a72d04d1f
lllytnik <papa.carlo.link@gmail.com>
2023-05-22 20:00:35 +0400

    deleted comment search

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,24 +1,3 @@
-// const searchForm = document.querySelector('.search-form');
-// searchForm.addEventListener('submit', handleSearch);
-
-// function handleSearch(event) {
-//     event.preventDefault();
-
-//     const searchInput = document.querySelector('.search-input');
-//     const searchTerm = searchInput.value.toLowerCase();
-//     const products = Array.from(document.querySelectorAll('.product-card'));
-
-//     for (const product of products) {
-//         const productTitle = product.querySelector('.product-card__title');
-//         const productName = productTitle.textContent.toLowerCase();
-
-//         if (productName.includes(searchTerm)) {
-//             product.style.display = 'flex';
-//         } else {
-//             product.style.display = 'none';
-//         }
-//     }
-// }
 const searchForm = document.querySelector('.search-form');
 const searchInput = document.querySelector('.search-input');
 let timeoutId;
